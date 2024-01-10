75e51f770667c89257fef6ead3e6f502c8846e3d
MohammedSadok <mohammed.sadok.2003@gmail.com>
2024-01-10 22:43:14 +0100

    set up navigation and authentication

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+type Props = {};
 
-export default function TabOneScreen() {
+const Page = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      <Text>hello</Text>
+      <Link href={"/(modals)/login"} >
+        login
+      </Link>
+      <Link href={"/(modals)/booking"} >
+        Bookings
+      </Link>
+      <Link href={"/listing/list"} >
+        Listing
+      </Link>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default Page;
+
+const styles = StyleSheet.create({});
